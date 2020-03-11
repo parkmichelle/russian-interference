49edcda6350ede549aae8a8896fe1bd60f253e11
Stephanie Niu <niustephanie@gmail.com>
2020-03-10 18:05:03 -0700

    added todos

--- rehydrate_tweets.js
+++ rehydrate_tweets.js
@@ -12,21 +12,12 @@ let amelie_plot = amelie_scroll.append('g')
 let ten_gop_plot = ten_gop_scroll.append('g')
 
 d3.csv(dataDir + tweetsFilename).then(function(data) {
-  amelieData = [];
-  tenGopData = [];
-  data.forEach(element => {
-    //if (element.user_key == "ameliebaldwin") amelieData.append(element);
-    //else tenGopData.append(element);
-    drawTweet(element)
-  });
-  
-  //drawTweets(origData);
+  data.forEach(element => {drawTweet(element)});
 });
 
-// Generate wordcloud credits: http://using-d3js.com/07_01_word_clouds.html
 function drawTweet(row) {
-  var id = "#";
   var plot;
+  // Populate the username
   if (row.user_key == "ameliebaldwin") {
     amelie_plot.append('tspan')
       .attr('class', 'tweet h1')
@@ -39,32 +30,24 @@ function drawTweet(row) {
       .text("Tennessee");
     plot = ten_gop_plot;
   }
+
+  // Populate the rest of the tweet
   plot.append('tspan')
       .attr('class', 'tweet h2')
       .text("   @" + row.user_key + " | ");
   plot.append('tspan')
       .attr('class', 'tweet h2')
-      .text(row.created_str);
+      .text(row.created_str); // TODO format date in Month, Day format
   plot.append('p')
       .attr('class', 'tweet body')
       .text(row.text);
   plot.append('tspan')
       .attr('class', 'tweet h2')
-      .text("Retweets: " + row.retweet_count);
+      .text("Retweets: " + row.retweet_count); // TODO add retweet/favorite icon
   plot.append('tspan')
       .attr('class', 'tweet h2')
-      .text("     Favorites: " + row.favorite_count);
+      .text("     Favorites: " + row.favorite_count); // TODO handle missing values
   plot.append('hr')
-
-  /*let text = plot.selectAll('text');
-  let updatedText = text.data(tweetData, d => d.text);
-
-  let enterSelection = updatedText.enter();
-  let newText = enterSelection.append('text')
-          .attr('x', 50)
-          .attr('y', 50)
-          .style('font-size', 12);
-  updatedText.exit().remove();*/
 }
 
 
