60a538de6c711170787914947eda93790522d00d
Stephanie Niu <niustephanie@gmail.com>
2020-03-12 01:17:53 -0700

    hovering worked! styles and fonts added

--- draw_scatter.js
+++ draw_scatter.js
@@ -4,34 +4,58 @@ let plotHeight = 500;
 let plotMargin = 75;
 let outerWidth = plotWidth + 2 * plotMargin;
 let outerHeight = plotHeight + 2 * plotMargin; 
-// Select the `<svg id="animal-viz"></svg>` DOM node
+
+const font = "Work Sans";
+
 let wholeChart = d3.select('#users-overview'); 
 wholeChart
   .attr('width', outerWidth)
   .attr('height', outerHeight); 
-// Create a `g` element to our SVG: we'll work with this for our plot
+// Create a `g` element to our SVG
 let plot = wholeChart.append('g')
   .attr('transform', `translate(${plotMargin},${plotMargin})`); 
-// Create our scales, each of which will map data from 0-10 to the size of our plot
+
+var tooltip = d3.select(".tooltip");
+
+var tipMouseover = function(d) {
+    var html  = "<span class='tooltip-header'>" + "@" + d.screen_name + "</span>" + 
+    "<br/>" + "Followers: " + "<span class='tooltip-text'>" + 
+    d.followers_count + "</span>"+ "<br/>" + "Favorites: " + "<span class='tooltip-text'>"
+    + d.favourites_count + "</span>";
+    tooltip.html(html)
+        .style("left", (d3.event.pageX + 10) + "px")
+        .style("top", (d3.event.pageY - 60) + "px")
+      .transition()
+        .duration(0) // ms
+        .style("opacity", .9) // started as 0!
+};
+// tooltip mouseout event handler
+var tipMouseout = function(d) {
+    tooltip.transition()
+        .duration(0) // ms
+        .style("opacity", 0); // don't care about position!
+};
+
+// Create our scales, each of which will map data to the size of our plot
 let xScale = d3.scaleLinear()
   .domain([0, 100000])
-  .range([0, plotWidth]);
+  .range([0, plotWidth])
 let yScale = d3.scaleLinear()
   .domain([0, 25000])
-  .range([plotHeight, 0]); // SVG has its origin in the top left, so we need to be careful here! 
-// xScale and yScale are functions:
-// xScale(0) => 0; xScale(10) => 500; xScale(2) => 100
-// yScale(0) => 500; yScale(10) => 0; yScale(2) => 400 
+  .range([plotHeight, 0]); 
+
 // Draw our axes based on xScale and yScale
 let xAxis = plot.append('g')
   .attr('transform', `translate(0,${plotHeight})`)
   .call(d3.axisBottom(xScale));
 let yAxis = plot.append('g')
   .call(d3.axisLeft(yScale)); 
+
 // label the axes
 plot.append("text")             
 .attr("transform", `translate(${plotWidth/2}, ${plotHeight + 35})`)
 .style("text-anchor", "middle")
+.style("font-family", font)
 .text("Followers"); 
 plot.append("text")
   .attr("transform", "rotate(-90)")
@@ -39,7 +63,17 @@ plot.append("text")
   .attr("x",0 - (plotHeight / 2))
   .attr("dy", "1em")
   .style("text-anchor", "middle")
+  .style("font-family", font)
   .text("Favorites on Tweets");     
+
+// Add title
+plot.append("text")
+  .attr("x", (width / 3))             
+  .attr("y", 0 - (plotMargin / 2))
+  .attr("text-anchor", "middle")  
+  .style("font-size", "24px") 
+  .style("font-family", font)  
+  .text("Popularity of Troll Profiles");
 
 d3.csv('data/users.csv').then(function(data){
     window.allData = data;
@@ -49,9 +83,8 @@ d3.csv('data/users.csv').then(function(data){
     drawScatterPlot(data);
 }); 
 
-// Convert weight and height from strings to numbers
+// Convert values from strings to numbers
 function parseInputRow(d) {
-  //console.log(d);
   return {
     id: +d.id,
     followers_count: +d.followers_count,
@@ -62,38 +95,19 @@ function parseInputRow(d) {
 };
 
 function drawScatterPlot(userData) {
-  // Create a selection of circles in our plot (empty to start)
   let circles = plot.selectAll('circle'); 
-  // Bind our animal data to the circles
   let updatedCircles = circles.data(userData, d => d.id); 
-  // Can also set the id to "name"! The key for each datapoint can be anything.
-  // Ideally, this key should be unique.
-  // By default, D3 uses the index in the data array, in our example that won't work.
-  // let updatedCircles = circles.data(animalData, d => d.name); 
-  // We'll use "enter" to make circles for new datapoints (in this case, all datapoints)
-  // From https://github.com/d3/d3-selection#selection_enter :
-  // "The enter selection is typically used to create 'missing' elements corresponding to new data."
-  // "[The selection comprises] placeholder nodes for each datum that had no corresponding DOM elementin the selection."
-  // "Conceptually, the enter selection’s placeholders are pointers to the parent element"
   let enterSelection = updatedCircles.enter();
   let newCircles = enterSelection.append('circle')
-    .attr('r', function (d) { return d.statuses_count * .0005; })
+    // Uses an exponent because # statuses scales exponentially
+    .attr('r', function (d) { return Math.pow(d.statuses_count, .9)*.0005; })
     .attr('cx', function (d) { return xScale(d.followers_count); })
     .attr('cy', function (d) { return yScale(d.favourites_count); })
-    //.attr("fill-opacity","0.5")
-    .style("stroke","midnightblue")
-    .style("stroke-width",".5px")
-    .style('fill', 'steelblue');
+    .attr("fill-opacity","0")
+    .style("stroke","steelblue")
+    .style("stroke-width","2px")
+    .on("mouseover", tipMouseover)
+    .on("mouseout", tipMouseout);
   updatedCircles.exit().remove();
 
 };
-
-  /*let buttons = document.querySelectorAll('button');
-  for (let b of buttons) {
-    b.addEventListener('click', function() {
-      let newData = allData;
-      if (b.dataset.filter !== 'both') {
-        newData = allData.filter(d => d.animal === b.dataset.filter);
-      }    
-      drawScatterPlot(newData);
-       });*/
